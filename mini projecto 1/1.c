966ea2c767fdb91f6d9000a6e88d99aa8844764c
Telmo Panaças <telmo@DESKTOP-JFQ3BO3.localdomain>
2021-05-11 23:04:23 +0100

    27/40

--- mini projecto 1/1.c
+++ mini projecto 1/1.c
@@ -6,13 +6,19 @@
 void menu();
 void preencherMatriz(char matrizMapa[25][25]);
 void limparMapa(char matrizMapa[25][25]);
+int verFicheiroTexto(char file_name[MAX]);
+
 
 int main(void) {
-    char input = '>';
+    
     char opcao[MAX], file_name[MAX], linhaExport[MAX],xExport[MAX], yExport[MAX],cExport[MAX];
     FILE *ficheiroOriginal, *ficheiroExport;
-    int podesLerDimensoes = 0, nLin = 0, nCol = 0, xLinha, xColuna,
-        triggerX = 0, triggerY = 0, plantX = 0, plantY = 0;
+    int podesLerDimensoes = 0,triggerX = -1, triggerY = -1, plantX = -1, plantY = -1;
+    
+    char linhaCopiada[MAX];
+    char file_name_export[MAX];
+
+    
     
     char matrizMapa[25][25] = {
         {'\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0'},
@@ -43,24 +49,31 @@ int main(void) {
     
     /*Primeiro menu*/
     menu();
-    char linhaCopiada[MAX], file_name_export[MAX];
+    
     while (1)
     {        
         putchar('>');
-        scanf(" %s", opcao);
+        fgets(opcao,128,stdin);
         
-        if (strcmp(opcao, "quit") == 0)
+        if (strncmp(opcao, "quit", 4) == 0)
         {
 
             return 0;
 
-        }else if(strcmp(opcao, "sos") == 0){
+        }else if(strncmp(opcao, "sos", 3) == 0){
             
             menu();
 
-        }else if (strcmp(opcao, "export") == 0)
+        }else if (strncmp(opcao, "export", 6) == 0)
         {
-            scanf(" %s", file_name_export);
+            int k,n;
+            n = sscanf(opcao,"%*s %s", file_name_export);
+            if (n != 1)
+            {
+                puts("Invalid command!");
+                continue;
+            }
+            
             ficheiroExport = fopen(file_name_export, "w");
             if (ficheiroExport == NULL)
             {
@@ -68,54 +81,70 @@ int main(void) {
                 puts("Error opening file");
                 exit(EXIT_FAILURE);
             }
-            
-            for (int k = 0; k < 1; k++)
+            for (k = 0; k < 25; k++)
             {
-                for (int u = 0; u < 3; u++)
+                int u;
+                for (u = 0; u < 25; u++)
                 {
                     /*NAO ESTÁ A COPIAR BEM O CARACTER*/
                     sprintf(xExport, " %d", k);
                     sprintf(yExport, " %d", u);
                     cExport[0] = matrizMapa[k][u];
                     
+                    /*
+                    printf("%s\n", cExport);
+                    puts(xExport);
                     puts(yExport);
-                    cExport[0] = '\0';
-                    strcat(cExport, xExport);
-                    strcat(cExport, yExport);
-                    strcpy(linhaExport, cExport);
+                    */
+                    strcpy(linhaExport,cExport);
+                    strcat(linhaExport, " ");
+                    strcat(linhaExport,xExport);
+                    strcat(linhaExport, " ");
+                    strcat(linhaExport, yExport);
+                    
+                    
                     fputs(linhaExport, ficheiroExport);
+                    fputc('\n',ficheiroExport);
                     
                 }
                 
             }
             
             fclose(ficheiroExport);
-            puts("O ficheiro foi exportado com sucesso");
             
-        }else if(strcmp(opcao, "read") == 0)
-        {
-            scanf(" %s", file_name);
-            puts(file_name);
+        }else if(strncmp(opcao, "read", 4) == 0)
+        {    
+            int n,k,linha,coluna;
+            char tipoBomba;
+            n = sscanf(opcao,"%*s %s", file_name);
+            if (n != 1)
+            {
+                puts("Invalid command!");
+                continue;
+            }
+            
             ficheiroOriginal = fopen(file_name, "r");
             if (ficheiroOriginal == NULL)
+            {                
+                puts("Error openning file");
+                exit(EXIT_FAILURE);
+            } else if (verFicheiroTexto(file_name) == 1)
             {
+                                    /*ACHO QUE ISTO TRATA TO ERRO 34*/
                 puts("Error openning file");
                 exit(EXIT_FAILURE);
             } else {
-                printf("\nO ficheiro %s foi aberto corretamente\n", file_name);
 
                 /*Ler até ser lido as dimensões do ficheiro*/
                 while (podesLerDimensoes == 0 && fgets(linhaCopiada, sizeof(linhaCopiada), ficheiroOriginal))
                 {
                     
-                    if (linhaCopiada[0] == '#')
+                    if (linhaCopiada[0] == '#' || linhaCopiada[0] == '\n')
                     {
                         continue;                        
                     }
-                    char *nLinhas = strtok(linhaCopiada, " ");
-                    nLin = atoi(nLinhas);
-                    char *nColunas = strtok(NULL, " ");
-                    nCol = atoi(nColunas);
+                    
+                
                     podesLerDimensoes++;
                 }
                          
@@ -123,86 +152,99 @@ int main(void) {
                 /*Aqui é lido o resto e adicionado a matriz*/
                 while (fgets(linhaCopiada, sizeof(linhaCopiada), ficheiroOriginal))
                 {
-                    if (linhaCopiada[0] == '#')
+
+                    /*PORQUE É QUE NÃO ESTÁ A IGNORAR AS LINHAS BRANCAS?*/
+
+                    if (linhaCopiada[0] == '#' || linhaCopiada[0] == '\n')
                     {
                         continue;                        
-                    }
-                    /*Separar as informações*/
-                    char *tipoBomba = strtok(linhaCopiada, " ");
-                    char *linha = strtok(NULL, " ");
-                    char *coluna = strtok(NULL, " ");
-                    
-                    if (coluna == NULL || linha == NULL)
-                    {
-                        puts("\nFile is corrupted");
-                        limparMapa(matrizMapa);
-                        fclose(ficheiroOriginal);
                     }else{
+                        /*Separar as informações*/
+                        k = sscanf(linhaCopiada, " %c %d %d", &tipoBomba, &linha, &coluna);
+                        if (k != 3)
+                        {
+                            puts("File is corrupted");
+                            limparMapa(matrizMapa);
+                            fclose(ficheiroOriginal);
+                        }else{
                     
-                    xLinha = atoi(linha);
-                    xColuna = atoi(coluna);
-                    if (xLinha < 0 || xColuna < 0 || xLinha > 25 || xColuna > 25)
-                    {
-                        puts("\nFile is corrupted");
-                        limparMapa(matrizMapa);
-                        fclose(ficheiroOriginal);
-                    }
-                    
-                    matrizMapa[xLinha][xColuna] = tipoBomba[0];
-                    }
-                    
+                            if (linha < 0 || coluna < 0 || linha > 24 || coluna > 24)
+                            {
+                                puts("File is corrupted");
+                                limparMapa(matrizMapa);
+                                fclose(ficheiroOriginal);
+                            }else {
+                                matrizMapa[linha][coluna] = tipoBomba;
+                            }
+                        }
+                    }  
                 }               
               
             }
             fclose(ficheiroOriginal);           
             preencherMatriz(matrizMapa);
-        }else if(strcmp(opcao, "show") == 0)
+        }else if(strncmp(opcao, "show", 4) == 0)
         {
-            for (int linhas = 0; linhas < 25; linhas++)
+            int linhas;
+            preencherMatriz(matrizMapa);
+            
+            for (linhas = 0; linhas < 25; linhas++)
             {
-                for (int colunas = 0; colunas < 25; colunas++)
+                int colunas;
+                for (colunas = 0; colunas < 25; colunas++)
                 {
                     printf("%c", matrizMapa[linhas][colunas]);
                 }
                 putchar('\n');
             }
-        }else if(strcmp(opcao, "trigger") == 0)
+        }else if(strncmp(opcao, "trigger", 7) == 0)
         {
-            scanf(" %d", &triggerX);
-            scanf(" %d", &triggerY);
-            
-            if (triggerX > 25 || triggerY >25 || triggerX < 0 || triggerY < 0)
+            int n;
+            n = sscanf(opcao,"%*s %d %d", &triggerX, &triggerY);
+            if (n != 2)
             {
                 puts("Invalid coordinate");
+                continue;
             }
             
-            if (matrizMapa[triggerX][triggerY] == '.')
-            {
-                matrizMapa[triggerX][triggerY] = '*';
-            }
-
-            if (matrizMapa[triggerX][triggerY] != '.' && matrizMapa[triggerX][triggerY] != '*')
-            {
-                puts("There is no bomb at the specified coordinate");
-            }
-            
-        }else if (strcmp(opcao, "plant") == 0)
-        {
-            scanf(" %d", &plantX);
-            scanf(" %d", &plantY);
-
-            if (plantX > 25 || plantY >25 || plantX < 0 || plantY < 0)
+            if (triggerX > 24 || triggerY >24 || triggerX < 0 || triggerY < 0)
             {
                 puts("Invalid coordinate");
+            }else {
+        
+                if (matrizMapa[triggerX][triggerY] == '.')
+                {
+                    matrizMapa[triggerX][triggerY] = '*';
+                }
+
+                if (matrizMapa[triggerX][triggerY] != '.' && matrizMapa[triggerX][triggerY] != '*')
+                {
+                    puts("No mine at specified coordinate");
+                }
+            }
+            
+        }else if (strncmp(opcao, "plant",5) == 0)
+        {
+            int n;
+            n = sscanf(opcao, "%*s %d %d", &plantX, &plantY);
+            if (n != 2)
+            {
+                puts("Invalid coordinate");
+                continue;
             }
 
-            if (matrizMapa[plantX][plantY] == '*')
+            if (plantX > 24 || plantY >24 || plantX < 0 || plantY < 0 )
             {
-                matrizMapa[plantX][plantY] = '.';
+                puts("Invalid coordinate");
+            }else{
+                if (matrizMapa[plantX][plantY] == '*')
+                {
+                    matrizMapa[plantX][plantY] = '.';
+                }
             }
+            
         }else {
-            puts("There is no such option, returning to menu");
-            menu();
+            puts("Invalid command!");        
         }
 
     }
@@ -210,34 +252,24 @@ int main(void) {
 }
 
 void menu(){
-    char menu[9][54]={
-        {'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'},
-        {'r','e','a','d',' ','<','f','i','l','e','n','a','m','e','>',' ',' ',' ',' ',' ','-',' ','r','e','a','d',' ','i','n','p','u','t',' ','f','i','l','e'},
-        {'s','h','o','w',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','-',' ','s','h','o','w',' ','t','h','e',' ','m','i','n','e',' ','m','a','p'},
-        {'t','r','i','g','g','e','r',' ','<','x','>',' ','<','y','>',' ',' ',' ',' ',' ','-',' ','t','r','i','g','g','e','r',' ','m','i','n','e',' ','a','t',' ','<','x','>',' ','<','y','>'},
-        {'p','l','a','n','t',' ','<','x','>',' ','<','y','>',' ',' ',' ',' ',' ',' ',' ','-',' ','p','l','a','c','e',' ','a','r','m','e','d',' ','m','i','n','e',' ','a','t',' ','<','x','>',' ','<','y','>'},
-        {'e','x','p','o','r','t',' ','<','f','i','l','e','n','a','m','e','>',' ',' ',' ','-',' ','s','a','v','e',' ','f','i','l','e',' ','w','i','t','h',' ','c','u','r','r','e','n','t',' ','m','a','p'},
-        {'q','u','i','t',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','-',' ','e','x','i','t',' ','p','r','o','g','r','a','m'},
-        {'s','o','s',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','-','s','h','o','w',' ','m','e','n','u'},
-        {'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'}};
-
-    putchar('\n');
-    for (int i = 0; i < 9; i++)
-    {
-        for (int j = 0; j < 54 ; j++)
-        {
-            printf("%c", menu[i][j]);
-        }
-        putchar('\n');
-    }
+   puts("+-----------------------------------------------------");
+   puts("read <filename>     - read input file");
+   puts("show                - show the mine map");
+   puts("trigger <x> <y>     - trigger mine at <x> <y>");
+   puts("plant <x> <y>       - place armed mine at <x> <y>");
+   puts("export <filename>   - save file with current map");
+   puts("quit                - exit program");
+   puts("sos                 - show menu");
+   puts("+-----------------------------------------------------");
 }
 
 void preencherMatriz(char matrizMapa[25][25])
 {
-    
-    for (int linhas = 0; linhas < 25; linhas++)
+    int linhas;
+    for (linhas = 0; linhas < 25; linhas++)
     {
-        for (int colunas = 0; colunas < 25; colunas++)
+        int colunas;
+        for (colunas = 0; colunas < 25; colunas++)
         {
             if (matrizMapa[linhas][colunas] == '\0')
             {
@@ -249,13 +281,31 @@ void preencherMatriz(char matrizMapa[25][25])
 
 void limparMapa(char matrizMapa[25][25])
 {
-    for (int i = 0; i < 25; i++)
+    int i;
+    for (i = 0; i < 25; i++)
     {
-        for (int j = 0; j < 25; j++)
+        int j;
+        for (j = 0; j < 25; j++)
         {
             matrizMapa[i][j] = '\0';
         }
         
+    }
+    
+}
+
+int verFicheiroTexto(char file_name[MAX]){
+    int i;
+    char tipoFicheiro[MAX] = "";
+    for (i = strlen(file_name)-4; file_name[i] != '\0'; i++)
+    {
+        strncat(tipoFicheiro, &file_name[i], 1);
+    }
+    if (strcmp(tipoFicheiro, ".txt") == 0 || strcmp(tipoFicheiro, ".ini") == 0)
+    {
+        return 0;
+    }else {
+        return 1;
     }
     
 }
